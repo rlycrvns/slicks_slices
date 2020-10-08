73f1b7cda4e29fc5248c853cdf99b5f2f1c7dede
rlycrvns <rileycravens@MU-TH-UR.local>
2020-10-08 15:36:26 -0700

    add: slicemaster person pages

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -1,4 +1,4 @@
-import path from 'path';
+import path, { resolve } from 'path';
 import fetch from 'isomorphic-fetch';
 
 async function turnPizzasIntoPages({ graphql, actions }) {
@@ -103,6 +103,16 @@ async function turnSlicemastersIntoPages({ graphql, actions }) {
     }
   `);
   // 2. Turn each slicemaster into their own page
+  data.slicemasters.nodes.forEach((slicemaster) => {
+    actions.createPage({
+      component: resolve('./src/templates/Slicemaster.js'),
+      path: `/slicemaster/${slicemaster.slug.current}`,
+      context: {
+        name: slicemaster.person,
+        slug: slicemaster.slug.current,
+      },
+    });
+  });
   // 3. Figure out how many pages there are based on how many Slicemasters there are and how many per page
   const pageSize = parseInt(process.env.GATSBY_PAGE_SIZE);
   const pageCount = Math.ceil(data.slicemasters.totalCount / pageSize);
