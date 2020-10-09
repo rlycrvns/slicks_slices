38c9699584e8267358f79ec7c096b7f2dd20c1bc
rlycrvns <rileycravens@MU-TH-UR.local>
2020-10-09 12:00:11 -0700

    add: info fields

--- gatsby/src/pages/order.js
+++ gatsby/src/pages/order.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import SEO from '../components/SEO';
+import useForm from '../utils/useForm';
 
-export default function order() {
+export default function OrderPage() {
+  const { values, updateValue } = useForm({
+    name: '',
+    email: '',
+  });
+
   return (
     <>
       <SEO title="Order a Pizza!" />
-      <p>Hey! I'm the Order Page</p>
+      <form>
+        <fieldSet>
+          <legend>Your Info</legend>
+          <label htmlFor="name">Name</label>
+          <input id="name" type="text" name="name" value={values.name} onChange={updateValue} />
+          <label htmlFor="email">Email</label>
+          <input id="email" type="email" name="email" value={values.email} onChange={updateValue} />
+        </fieldSet>
+        <fieldSet>
+          <legend>Menu</legend>
+        </fieldSet>
+        <fieldSet>
+          <legend>Order</legend>
+        </fieldSet>
+      </form>
     </>
   );
 }
