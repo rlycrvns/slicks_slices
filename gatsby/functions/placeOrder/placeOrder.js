88367005a1fa55a0a8e724767babbf157db658e3
rlycrvns <rileycravens@MU-TH-UR.local>
2020-10-20 16:17:40 -0700

    add: generate email and validation funcs

--- gatsby/functions/placeOrder/placeOrder.js
+++ gatsby/functions/placeOrder/placeOrder.js
@@ -1,5 +1,32 @@
 const nodemailer = require('nodemailer');
 
+function generateOrderEmail({ order, total }) {
+  return `<div>
+    <h2>You Recent Order for ${total}</h2>
+    <p>We will have your order ready in the next 20 minutes.</p>
+    <ul>
+      ${order
+        .map(
+          (item) => `<li>
+        <img src="${item.thumbnail}" alt="${item.name}"/>
+        ${item.size} ${item.name} - ${item.price}
+      </li>`
+        )
+        .join('')}
+    </ul>
+    <p>Your total is <strong>${total}</strong> due at pickup</p>
+    <style>
+        ul {
+          list-style: none;
+          padding: 0;
+          margin: 0;
+        }
+        li {
+          margin-bottom: 15px;
+        }
+    </style>
+  </div>`;
+}
 // create a transport for nodemailer
 const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
@@ -11,16 +38,27 @@ const transporter = nodemailer.createTransport({
 });
 
 exports.handler = async (event, context) => {
-  // test send an email
+  const body = JSON.parse(event.body);
+  // validate the data coming in is correct
+  const requiredFields = ['email', 'name', 'order'];
+
+  for (const field of requiredFields) {
+    if (!body[field]) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ message: `You are missing the ${field} field` }),
+      };
+    }
+  }
+  // send the email
   const info = await transporter.sendMail({
     from: 'Slicks Slices <slick@example.com>',
-    to: 'order@example.com',
+    to: `${body.name} <${body.email}>, orders@example.com`,
     subject: 'New Order!',
-    html: `<p>You new pizza order is here!</p>`,
+    html: generateOrderEmail({ order: body.order, total: body.total }),
   });
-  console.log(info);
   return {
     statusCode: 200,
-    body: JSON.stringify(info),
+    body: JSON.stringify({ message: 'Success' }),
   };
 };
