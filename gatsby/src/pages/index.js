3d08603f96e63ed8c4d2445db9651b846bd031d9
rlycrvns <rileycravens@MU-TH-UR.local>
2020-10-21 16:22:42 -0700

    add: home page content

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import LoadingGrid from '../components/LoadingGrid';
+import { HomePageGrid } from '../styles/Grids';
+import useLatestData from '../utils/useLatestData';
+
+function CurrentlySlicing({ slicemasters }) {
+  return (
+    <div>
+      {!slicemasters && <LoadingGrid count={4} />}
+      {slicemasters && !slicemasters?.length && <p>No one is working right now!</p>}
+    </div>
+  );
+}
+
+function HotSlices({ hotSlices }) {
+  return (
+    <div>
+      {!hotSlices && <LoadingGrid count={4} />}
+      {hotSlices && !hotSlices?.length && <p>Nothing in the case right now!</p>}
+    </div>
+  );
+}
 
 export default function HomePage() {
+  const { slicemasters, hotSlices } = useLatestData();
   return (
-    <>
-      <p>Hey! I'm the home page</p>
-    </>
+    <div className="center">
+      <h1>The Best Pizza Downtown!</h1>
+      <p>Open 11am to 11pm Every Single Day</p>
+      <HomePageGrid>
+        <CurrentlySlicing slicemasters={slicemasters} />
+        <HotSlices hotSlices={hotSlices} />
+      </HomePageGrid>
+    </div>
   );
 }
