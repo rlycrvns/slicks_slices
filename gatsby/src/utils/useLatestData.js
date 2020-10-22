2c35983bf903c9b80bbf6f6eef12a3b437c76228
rlycrvns <rileycravens@MU-TH-UR.local>
2020-10-22 14:44:23 -0700

    add: query var/ string.raw trick

--- gatsby/src/utils/useLatestData.js
+++ gatsby/src/utils/useLatestData.js
@@ -1,10 +1,25 @@
 import { useEffect, useState } from 'react';
 
+const gql = String.raw;
+
+const info = gql`
+  name
+  _id
+  image {
+    asset {
+      url
+      metadata {
+        lqip
+      }
+    }
+  }
+`;
+
 export default function useLatestData() {
   // slicesmasters
   const [slicemasters, setSlicemasters] = useState();
   // hot slices
-  const [hotSlices, setHostSlices] = useState();
+  const [hotSlices, setHotSlices] = useState();
   // use a side effect to fetch the data from the graphql endpoint
   useEffect(function () {
     // when the component loads, fetch the data
@@ -14,27 +29,29 @@ export default function useLatestData() {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        query: `
-        query{
-          StoreSettings(id: "downtown") {
-            name
-            slicemaster {
+        query: gql`
+          query {
+            StoreSettings(id: "downtown") {
               name
-            }
-            hotSlices {
-              name
+              slicemaster {
+                ${info}
+              }
+              hotSlices {
+                ${info}
+              }
             }
           }
-        }
         `,
       }),
     })
       .then((res) => res.json())
       .then((res) => {
-        // check for errors
         // set the data state
         setSlicemasters(res.data.StoreSettings.slicemaster);
-        setHostSlices(res.data.StoreSettings.hotSlices);
+        setHotSlices(res.data.StoreSettings.hotSlices);
+      })
+      .catch((err) => {
+        console.log(err);
       });
   }, []);
   return {
