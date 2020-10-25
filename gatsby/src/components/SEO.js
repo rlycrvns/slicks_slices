5be4bcca37004d389b303f4b6df3f23d3a38447a
rlycrvns <rileycravens@MU-TH-UR.local>
2020-10-25 14:10:48 -0700

    add: viewport minimum scale

--- gatsby/src/components/SEO.js
+++ gatsby/src/components/SEO.js
@@ -22,7 +22,7 @@ export default function SEO({ children, location, description, title, image }) {
       <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
       <link rel="alternate icon" href="/favicon.ico" />
       {/* Meta Tags */}
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1" />
       <meta charSet="utf-8" />
       <meta name="description" content={site.siteMetadata.description} />
       {/* Open Graph */}
